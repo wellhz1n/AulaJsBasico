a9a055d800878535f3da2719794ae46801a2858c
Wellingtom Ramos <wellingtom.h@hotmail.com>
2020-01-20 21:00:22 -0300

    Aula02

---
+++ Aulas/Aula02/index.js
@@ -0,0 +1,18 @@
+
+var variavelGlobal = "Global";
+
+if (variavelGlobal != null) {
+    var variavelIf = true;
+}
+else {
+    let variavelLet = false;
+}
+
+function FunctionTeste(x, y) {
+    let resultado = x + y;
+    return resultado;
+}
+alert(FunctionTeste(5,8));
+// Var != let
+// Funcoes
+
